5919c92c93361986720b6e7876491d4422793e65
selva-kumar-s <85547328+selva-kumar-s@users.noreply.github.com>
2026-03-05 06:29:40 +0000

    commit

--- Learn/inventory.py
+++ Learn/inventory.py
@@ -9,20 +9,20 @@ new_book = {
 }
 
 # ---------- Task 1: Read the inventory ----------
-with open("inventory.json", "r", encoding="utf-8") as file:
+with open("C:/Users/selva_kumar/Downloads/inventory.json", "r", encoding="utf-8") as file:
     inventory = json.load(file)
 
 print(f"Total books currently in the file: {len(inventory)}")
 
-# # ---------- Task 2: Update and save ----------
-# inventory.append(new_book)
+# ---------- Task 2: Update and save ----------
+inventory.append(new_book)
 
-# with open("inventory.json", "w", encoding="utf-8") as file:
-#     json.dump(inventory, file, indent=4)
+with open("C:/Users/selva_kumar/Downloads/inventory.json", "w", encoding="utf-8") as file:
+    json.dump(inventory, file, indent=4)
 
-# # ---------- Task 3: Display the updated inventory ----------
-# with open("inventory.json", "r", encoding="utf-8") as file:
-#     updated_inventory = json.load(file)
+# ---------- Task 3: Display the updated inventory ----------
+with open("C:/Users/selva_kumar/Downloads/inventory.json", "r", encoding="utf-8") as file:
+    updated_inventory = json.load(file)
 
-# for book in updated_inventory:
-#     print(f"Title: {book['title']} | Author: {book['author']} | Price: ${book['price']:.2f}")
+for book in updated_inventory:
+    print(f"Title: {book['title']} | Author: {book['author']} | Price: ${book['price']:.2f}")
